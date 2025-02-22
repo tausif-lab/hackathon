de7bd06c31289226ce11c4ad10b5aefed0ba606f
tausif-lab <tautumhare@gmail.com>
2025-02-22 11:26:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+
+    function revealSections() {
+        sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (sectionTop < windowHeight - 100) {
+                section.classList.add("show");
+            } else {
+                section.classList.remove("show");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", revealSections);
+    revealSections();
+});
+
+
+
+
